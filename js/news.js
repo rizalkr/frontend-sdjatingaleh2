92831c2da54c389035568e55063c226cafbdeb05
Rizal Kurnia <klontongrizal@gmail.com>
2025-03-18 15:16:36 +0700

    edit news date agar lebih mudah diabaca

--- js/news.js
+++ js/news.js
@@ -10,7 +10,12 @@ $(document).ready(function(){
         $newsContainer.empty();
         if (data && data.length) {
           data.forEach((newsItem, index) => {
-            // Menggunakan struktur yang sama dengan style berita yang ada
+            // Convert createdAt to a readable date string in Indonesian locale
+            const readableDate = new Date(newsItem.createdAt).toLocaleDateString('id-ID', {
+              day: 'numeric',
+              month: 'long',
+              year: 'numeric'
+            });
             let card = `
               <div class="col-lg-6 mb-4" data-aos="fade-up" data-aos-delay="${(index % 2 === 0) ? '100' : '200'}">
                 <div class="media-h d-flex h-100">
@@ -20,7 +25,7 @@ $(document).ready(function(){
                   <div class="media-h-body">
                     <h2 class="mb-3"><a href="#">${newsItem.title}</a></h2>
                     <div class="meta">
-                      <span class="icon-calendar mr-2"></span><span>${newsItem.createdAt}</span>
+                      <span class="icon-calendar mr-2"></span><span>${readableDate}</span>
                       <span class="icon-person mr-2"></span>${newsItem.author}
                     </div>
                     <p>${newsItem.content}</p>
